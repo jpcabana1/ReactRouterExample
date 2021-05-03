ee5d8f0ee2acb618c61e7570a6aa6ebbe3e84b2a
João Pedro <jpcabana1@gmail.com>
2021-05-03 17:37:08 -0300

    router funcionando

--- router/src/App.js
+++ router/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/Home";
+import About from "./components/About";
+import Shop from "./components/Shop";
+import Error from "./components/Error";
+import Navbar from "./components/Navbar";
 
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
+        <Route component={Error} />
+      </Switch>
+    </main>
   );
 }
-
 export default App;
